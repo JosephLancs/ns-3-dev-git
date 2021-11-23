42c33f696520fe73701d25b290da8f780f8de6da
JosephLancs <j.deakin@lancaster.ac.uk>
2021-11-23 13:49:24 +0000

    merge

---
+++ ns-3.30.1/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/subdir/ns3.30.1-subdir-debug', 'build/scratch/ns3.30.1-imp-debug', 'build/scratch/ns3.30.1-tut7-debug', 'build/scratch/ns3.30.1-scratch-simulator-debug', 'build/scratch/ns3.30.1-manet-debug', 'build/scratch/ns3.30.1-blackhole-debug', 'build/scratch/ns3.30.1-tut1-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
